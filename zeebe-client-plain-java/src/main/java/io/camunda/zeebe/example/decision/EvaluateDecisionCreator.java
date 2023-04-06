34409147d44e7fd02fdd5808b55293d70d5078e3
Aleksander Dytko <102789122+aleksander-dytko@users.noreply.github.com>
2023-04-06 17:13:05 +0200

    Refactor EvaluateDecisionCreator.java

--- zeebe-client-plain-java/src/main/java/io/camunda/zeebe/example/decision/EvaluateDecisionCreator.java
+++ zeebe-client-plain-java/src/main/java/io/camunda/zeebe/example/decision/EvaluateDecisionCreator.java
@@ -10,28 +10,31 @@ package io.camunda.zeebe.example.decision;
 import io.camunda.zeebe.client.ZeebeClient;
 import io.camunda.zeebe.client.ZeebeClientBuilder;
 import io.camunda.zeebe.client.api.response.EvaluateDecisionResponse;
-import io.camunda.zeebe.client.api.response.ProcessInstanceEvent;
 
 public final class EvaluateDecisionCreator {
 
   public static void main(final String[] args) {
     final String defaultAddress = "localhost:26500";
 
-    final ZeebeClient client = ZeebeClient.newClientBuilder().credentialsProvider(new MyCredentialsProvider()).gatewayAddress(defaultAddress).usePlaintext().build();
-
+    final ZeebeClientBuilder zeebeClientBuilder = ZeebeClient.newClientBuilder().credentialsProvider(new MyCredentialsProvider()).gatewayAddress(defaultAddress).usePlaintext();
 
     final String decisionId = "Decision_1aredx7";
 
-    final EvaluateDecisionResponse decisionEvaluation =
-            client
-                    .newEvaluateDecisionCommand()
-                    .decisionId(decisionId)
-                    .variables("{\"name\": \"Olek\"}")
-                    .send()
-                    .join();
+    System.out.println("Evaluating decision");
 
-    System.out.println("Decision evaluation result: " + decisionEvaluation.getDecisionOutput());
+    try (final ZeebeClient client = zeebeClientBuilder.build()) {
 
+      final EvaluateDecisionResponse decisionEvaluation =
+              client
+                      .newEvaluateDecisionCommand()
+                      .decisionId(decisionId)
+                      .variables("{\"name\": \"Olek\"}")
+                      .send()
+                      .join();
+
+      System.out.println("Decision evaluation result: " + decisionEvaluation.getDecisionOutput());
+
+    }
     }
   }
 
