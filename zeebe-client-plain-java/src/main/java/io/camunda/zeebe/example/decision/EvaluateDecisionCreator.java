8f903a4f9679ce1cb55d7e2422f36c5f3e2bb92f
Aleksander Dytko <102789122+aleksander-dytko@users.noreply.github.com>
2023-04-14 13:23:19 +0200

    Fixed zeebe api auth and resolved issue with logging

--- zeebe-client-plain-java/src/main/java/io/camunda/zeebe/example/decision/EvaluateDecisionCreator.java
+++ zeebe-client-plain-java/src/main/java/io/camunda/zeebe/example/decision/EvaluateDecisionCreator.java
@@ -11,14 +11,34 @@ import io.camunda.zeebe.client.ZeebeClient;
 import io.camunda.zeebe.client.ZeebeClientBuilder;
 import io.camunda.zeebe.client.api.response.DeploymentEvent;
 import io.camunda.zeebe.client.api.response.EvaluateDecisionResponse;
+import io.camunda.zeebe.client.impl.ZeebeClientBuilderImpl;
+import io.camunda.zeebe.client.impl.oauth.OAuthCredentialsProvider;
+import io.camunda.zeebe.client.impl.oauth.OAuthCredentialsProviderBuilder;
 
 public final class EvaluateDecisionCreator {
 
   public static void main(final String[] args) {
     final String defaultAddress = "localhost:26500";
 
+    final OAuthCredentialsProvider provider =
+            new OAuthCredentialsProviderBuilder()
+                    .clientId("zeebe")
+                    .clientSecret("zecret")
+                    .audience("zeebe-api")
+                    .authorizationServerUrl("http://localhost:18080/auth/realms/camunda-platform/protocol/openid-connect/token")
+                    .build();
+
+    final ZeebeClient client =
+            new ZeebeClientBuilderImpl()
+                    .gatewayAddress("localhost:26500").usePlaintext()
+                    .credentialsProvider(provider)
+                    .build();
+
+    System.out.println(client.newTopologyRequest().send().join().toString());
+
     //final ZeebeClientBuilder zeebeClientBuilder = ZeebeClient.newClientBuilder().credentialsProvider(new MyCredentialsProvider()).gatewayAddress(defaultAddress).usePlaintext();
 
+    /*
     final ZeebeClientBuilder zeebeClientBuilder = ZeebeClient.newClientBuilder().gatewayAddress(defaultAddress).usePlaintext();
 
     final String decisionId = "Decision_1xuoniq";
@@ -44,6 +64,8 @@ public final class EvaluateDecisionCreator {
       System.out.println("Decision evaluation result: " + decisionEvaluation.getDecisionOutput());
 
     }
+
+     */
     }
   }
 
